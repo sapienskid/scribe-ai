919e66e3fc2b412b97fe8fd0d7e43555fa22598f
Sabin Pokharel <savinpokharel@gmail.com>
2024-10-30 10:41:00 +0545

    add  add_key, remove_key, update_key, save_keys, add_google_key, remove_google_key method

--- scribe_ai/utils/config.py
+++ scribe_ai/utils/config.py
@@ -25,7 +25,9 @@ class APIManager:
         logger.info(f"Loaded {len(storage_keys)} keys from storage: {[k[:5] + '...' for k in storage_keys]}")
         self.api_keys = env_keys + storage_keys
         logger.info(f"Total keys: {len(self.api_keys)}")
-        self.google_search_api_key = self.load_keys_from_env("GOOGLE_API_KEY")
+        self.google_search_api_keys = self.load_keys_from_env("GOOGLE_SEARCH_API_KEY")
+        self.google_search_engine_id_keys = self.load_keys_from_env("GOOGLE_SEARCH_ENGINE_ID")
+        self.google_api_keys= self.load_keys_from_env("GOOGLE_API_KEY")
     def load_keys_from_env(self, prefix):
         keys=[]
         main_key=os.getenv(prefix)
@@ -48,6 +50,53 @@ class APIManager:
         else:
             logger.warning("No keys available for switching.")
 
+    def add_key(self, new_key):
+        if new_key not in self.api_keys:
+            self.api_keys.append(new_key)
+            self.save_keys()
     
+    def remove_key(self, key_to_remove):
+        if key_to_remove in self.api_keys:
+            self.api_keys.remove(key_to_remove)
+            self.save_keys()
+    def update_key(self, old_key, new_key):
+        if old_key in self.api_keys:
+            index=self.api_keys.index(old_key)
+            self.api_keys[index]=new_key
+            self.save_keys()
+    
+    def save_keys(self):
+        env_keys = set(self.load_keys_from_env("GEMINI_API_KEY"))
+        keys_to_save =[key for key in self.api_keys if key not in env_keys]
+        api_storage.save_keys(keys_to_save)
+        api_storage.save_keys({
+            'google_search_api_key': self.google_search_api_keys,
+            'google_search_engine_id':self.google_search_engine_id_keys,
+            'google_api_keys': self.google_api_keys
+
+        })
+    def add_google_key(self, key_type, new_key):
+        if key_type =='search_api':
+            if new_key not in self.google_search_api_keys:
+                self.google_search_api_keys.append(new_key)
+        elif key_type =='search_engine_id':
+            if new_key not in self.google_search_engine_id_keys:
+                self.google_search_engine_id_keys.append(new_key)
+        elif key_type == 'api':
+            if new_key not in self.google_api_keys:
+                self.google_api_keys.append(new_key)
+    def remove_google_key(self, key_type, key_to_remove):
+        if key_type == 'search_api':
+            if key_to_remove in self.google_search_api_keys:
+                self.google_search_api_keys.remove(key_to_remove)
+        elif key_type == 'search_engine_id':
+            if key_to_remove in self.google_search_engine_id_keys:
+                self.google_search_engine_id_keys.remove(key_to_remove)
+        elif key_type == "api":
+            if key_to_remove in self.google_api_keys:
+                self.google_api_keys.remove(key_to_remove)
+
+
+
 
     

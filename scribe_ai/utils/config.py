dfd6624f64ea0061c76d5726baca96f9c1670d95
Sabin Pokharel <savinpokharel@gmail.com>
2024-10-30 09:47:22 +0545

    add imports

--- scribe_ai/utils/config.py
+++ scribe_ai/utils/config.py
@@ -1,1 +1,10 @@
-# TODO: Implement this module
+#config.py
+
+import os
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+from utils.api_storage import SecuredAPIStorage
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)

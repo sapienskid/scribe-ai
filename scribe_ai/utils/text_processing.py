fbb0c23d49cf45731e87cf388cc26114409c7367
Sabin Pokharel <savinpokharel@gmail.com>
2024-10-30 10:59:04 +0545

    add imports

--- scribe_ai/utils/text_processing.py
+++ scribe_ai/utils/text_processing.py
@@ -1,1 +1,8 @@
-# TODO: Implement this module
+#text_processing.py, this is the api wraper
+
+import logging
+import json
+import google.generativeai as genai
+from .config import api_manager, api_parameters
+
+import google.api_core.exceptions 
